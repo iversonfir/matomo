29eec3a38f74679fec8bdceceadc20a55a32c3a1
sgiehl <stefan@matomo.org>
2020-07-23 09:38:36 +0200

    Recalculate visit numbers dynamically in visitor profile when more visits are loaded

--- plugins/Live/javascripts/visitorProfile.js
+++ plugins/Live/javascripts/visitorProfile.js
@@ -226,6 +226,18 @@
                         self._showNoMoreVisitsSpan();
                     }
 
+                    var numbers = $('[data-number]');
+                    var max = $(numbers[0]).data('number');
+
+                    if (numbers.length > max) {
+                        var counter = numbers.length;
+                        numbers.each(function() {
+                            $(this).attr('data-number', counter);
+                            $('.counter', this).text(counter);
+                            counter--;
+                        });
+                    }
+
                     piwikHelper.lazyScrollTo($(response)[0], 400, true);
                 }
             });
